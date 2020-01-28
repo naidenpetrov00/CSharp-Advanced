375f6be0a43e0d9b3975560f503e96b1833c4223
Naiden Nikolaev Petrov <58266467+naidenpetrov00@users.noreply.github.com>
2020-01-28 22:07:27 +0200

    Exercise

---
+++ Functional Programming - Lab/01. Sort Even Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Sort_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var result = Console.ReadLine()
+                 .Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .Where(num => num % 2 == 0)
+                 .OrderBy(num => num)
+                 .ToList();
+
+            Console.WriteLine(string.Join(", ", result));   
+        }
+    }
+}
---
+++ Functional Programming - Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> parser = int.Parse;
+
+            var input = Console.ReadLine()
+                .Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(parser)
+                .ToArray();
+
+            Console.WriteLine(input.Length);
+            Console.WriteLine(input.Sum());
+        }
+    }
+}
