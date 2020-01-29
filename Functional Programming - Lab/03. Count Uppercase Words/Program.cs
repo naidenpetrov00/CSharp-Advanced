d54bf9b26785a5dca9095685d6085b26c848fc48
Naiden Nikolaev Petrov <58266467+naidenpetrov00@users.noreply.github.com>
2020-01-29 20:13:45 +0200

    Exercise

---
+++ Functional Programming - Lab/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Count_Uppercase_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine()
+                .Split()
+                .Where(word => word[0] == word.ToUpper()[0])
+                .ToList()
+                .ForEach(word => Console.WriteLine(word));
+        }
+    }
+}
---
+++ Functional Programming - Lab/04. Add VAT/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine()
+                .Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .Select(num => num * 1.2)
+                .ToList()
+                .ForEach(num => Console.WriteLine($"{num :F2}"));
+        }
+    }
+}
