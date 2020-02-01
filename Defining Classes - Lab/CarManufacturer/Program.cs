91c7fe3e7d78b5fd23959197dc26db7dcc4aeb9f
Naiden Nikolaev Petrov <58266467+naidenpetrov00@users.noreply.github.com>
2020-02-01 15:27:02 +0200

    Exercise

---
+++ Defining Classes - Lab/CarManufacturer/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CarManufacturer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+        }
+    }
+}
