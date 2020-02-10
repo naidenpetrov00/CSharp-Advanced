9492853559b10a61d4aed337a1248044fc93a3b9
Naiden Nikolaev Petrov <58266467+naidenpetrov00@users.noreply.github.com>
2020-02-11 00:08:58 +0200

    Exercise

---
+++ Generics - Exercise/Generic Box of String/Box.cs
@@ -0,0 +1,33 @@
+﻿namespace _01.GenericBoxofString
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Box<T>
+    {
+        private List<T> data;
+
+        public Box(List<T> element)
+        {
+            this.data = element;
+        }
+
+        public void ToString()
+        {
+            foreach (var item in data)
+            {
+                Console.WriteLine($"{item.GetType()}: {item}");
+            }
+        }
+
+        public void Swaper(int firstIndex, int secondIndex)
+        {
+            var firstValue = data[firstIndex];
+            var secondValue = data[secondIndex];
+
+            data[firstIndex] = secondValue;
+            data[secondIndex] = firstValue;
+        }
+    }
+}
---
+++ Generics - Exercise/Generic Box of String/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace _01.GenericBoxofString
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var num = int.Parse(Console.ReadLine());
+            var list = new List<int>();
+
+            for (int i = 0; i < num; i++)
+            {
+                list.Add(int.Parse(Console.ReadLine()));
+            }
+
+            Box<int> box = new Box<int>(list);
+
+            var swapNums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            box.Swaper(swapNums[0], swapNums[1]);
+
+            box.ToString();
+        }
+    }
+}
