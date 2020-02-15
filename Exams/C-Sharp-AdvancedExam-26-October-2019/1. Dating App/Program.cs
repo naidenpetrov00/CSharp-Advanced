438cd22ef74e93abbd9d4fec91fac9377feafddc
Naiden Nikolaev Petrov <58266467+naidenpetrov00@users.noreply.github.com>
2020-02-15 18:05:54 +0200

    ËxamExercise

---
+++ Exams/C-Sharp-AdvancedExam-26-October-2019/1. Dating App/Program.cs
@@ -0,0 +1,167 @@
+﻿namespace _1.DatingApp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] AllMales = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int[] AllFemales = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            var males = StackAdder(AllMales);
+            var females = QueueAdder(AllFemales);
+
+            var matchesCount = 0;
+
+            while (!EmptyStackChecker(males, females))
+            {
+                if (StackPeekBiggerThenZero(males) && QueuePeekBiggerThenZero(females))
+                {
+                    if (males.Peek() % 25 == 0 || females.Peek() % 25 == 0)
+                    {
+                        if (males.Peek() % 25 == 0)
+                        {
+                            males.Pop();
+                            if (EmptyStackChecker(males, females)) { break; }
+
+                            males.Pop();
+                            if (EmptyStackChecker(males, females)) { break; }
+                        }
+                        if (females.Peek() % 25 == 0)
+                        {
+                            females.Dequeue();
+                            if (EmptyStackChecker(males, females)) { break; }
+
+                            females.Dequeue();
+                            if (EmptyStackChecker(males, females)) { break; }
+                        }
+                        continue;
+                    }
+                }
+                if (!StackPeekBiggerThenZero(males) || !QueuePeekBiggerThenZero(females))
+                {
+                    if (!StackPeekBiggerThenZero(males))
+                    {
+                        males.Pop();
+                    }
+                    if (!QueuePeekBiggerThenZero(females))
+                    {
+                        females.Dequeue();
+                    }
+
+                    continue;
+                }
+
+                if (males.Peek() == females.Peek())
+                {
+                    matchesCount++;
+
+                    males.Pop();
+                    females.Dequeue();
+
+                    if (EmptyStackChecker(males, females)) { break; }
+                }
+                else
+                {
+                    females.Dequeue();
+
+
+                    int num = males.Pop();
+                    num -= 2;
+                    males.Push(num);
+                }
+            }
+
+            StringBuilder output = new StringBuilder();
+
+            output.AppendLine($"Matches: {matchesCount}");
+
+            if (males.Count == 0)
+            {
+                output.AppendLine("Males left: none");
+            }
+            else
+            {
+                output.AppendLine($"Males left: {string.Join(", ", males)}");
+            }
+
+            if (females.Count == 0)
+            {
+                output.AppendLine("Females left: none");
+            }
+            else
+            {
+                output.AppendLine($"Females left: {string.Join(", ", females)}");
+            }
+
+            Console.WriteLine(output);
+        }
+        static Stack<int> StackAdder(int[] males)
+        {
+            var stack = new Stack<int>();
+
+            foreach (var male in males)
+            {
+                stack.Push(male);
+            }
+
+            return stack;
+        }
+
+        static Queue<int> QueueAdder(int[] females)
+        {
+            var queue = new Queue<int>();
+
+            foreach (var female in females)
+            {
+                queue.Enqueue(female);
+            }
+
+            return queue;
+        }
+
+        static bool EmptyStackChecker(Stack<int> males, Queue<int> females)
+        {
+            if (males.Count == 0 || females.Count == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        static bool StackPeekBiggerThenZero(Stack<int> stack)
+        {
+            if (stack.Peek() > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static bool QueuePeekBiggerThenZero(Queue<int> queue)
+        {
+            if (queue.Peek() > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Exams/C-Sharp-AdvancedExam-26-October-2019/2.BookWorm/Program.cs
@@ -0,0 +1,220 @@
+﻿namespace _2.BookWorm
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var initialText = Console.ReadLine();
+
+            var size = int.Parse(Console.ReadLine());
+
+            char[,] field = MatrixAdder(size);
+
+            var moveCommand = string.Empty;
+            var position = PositionFinder(field);
+            int rowP = position[0];
+            int colP = position[1];
+
+            while (moveCommand != "end")
+            {
+                moveCommand = Console.ReadLine();
+                if (moveCommand == "end") { break; }
+
+                if (moveCommand == "up")
+                {
+                    if (SizeValidation(size, rowP - 1, colP))
+                    {
+                        if (field[rowP - 1, colP] != '-')
+                        {
+                            initialText += field[rowP - 1, colP];
+
+                            Mover(field, moveCommand, rowP, colP);
+                            rowP -= 1;
+                        }
+                        else
+                        {
+                            Mover(field, moveCommand, rowP, colP);
+                            rowP -= 1;
+                        }
+                    }
+                    else
+                    {
+                        if (initialText.Length != 0)
+                        {
+                            initialText = initialText.Remove(initialText.Length - 1);
+                        }
+                    }
+                }
+                else if (moveCommand == "down")
+                {
+                    if (SizeValidation(size, rowP + 1, colP))
+                    {
+                        if (field[rowP + 1, colP] != '-')
+                        {
+                            initialText += field[rowP + 1, colP];
+
+                            Mover(field, moveCommand, rowP, colP);
+                            rowP += 1;
+                        }
+                        else
+                        {
+                            Mover(field, moveCommand, rowP, colP);
+                            rowP += 1;
+                        }
+                    }
+                    else
+                    {
+                        if (initialText.Length != 0)
+                        {
+                            initialText = initialText.Remove(initialText.Length - 1);
+                        }
+                    }
+                }
+                else if (moveCommand == "left")
+                {
+                    if (SizeValidation(size, rowP, colP - 1))
+                    {
+                        if (field[rowP, colP - 1] != '-')
+                        {
+                            initialText += field[rowP, colP - 1];
+
+                            Mover(field, moveCommand, rowP, colP);
+                            colP -= 1;
+                        }
+                        else
+                        {
+                            Mover(field, moveCommand, rowP, colP);
+                            colP -= 1;
+                        }
+                    }
+                    else
+                    {
+                        if (initialText.Length != 0)
+                        {
+                            initialText = initialText.Remove(initialText.Length - 1);
+                        }
+                    }
+                }
+                else if (moveCommand == "right")
+                {
+                    if (SizeValidation(size, rowP, colP + 1))
+                    {
+                        if (field[rowP, colP + 1] != '-')
+                        {
+                            initialText += field[rowP, colP + 1];
+
+                            Mover(field, moveCommand, rowP, colP);
+                            colP += 1;
+                        }
+                        else
+                        {
+                            Mover(field, moveCommand, rowP, colP);
+                            colP += 1;
+                        }
+                    }
+                    else
+                    {
+                        if (initialText.Length != 0)
+                        {
+                            initialText = initialText.Remove(initialText.Length - 1);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(initialText);
+            Printer(field);
+        }
+
+        static char[,] MatrixAdder(int size)
+        {
+            char[,] matrix = new char[size, size];
+            var row = new char[] { };
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                row = Console.ReadLine().ToCharArray();
+
+                for (int k = 0; k < matrix.GetLength(1); k++)
+                {
+                    matrix[i, k] = row[k];
+                }
+            }
+
+            return matrix;
+        }
+
+        static int[] PositionFinder(char[,] matrix)
+        {
+            var position = new int[2];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+
+                for (int k = 0; k < matrix.GetLength(1); k++)
+                {
+                    if (matrix[i, k] == 'P')
+                    {
+                        position[0] = i;
+                        position[1] = k;
+
+                        break;
+                    }
+                }
+            }
+
+            return position;
+        }
+
+        static bool SizeValidation(int size, params int[] position)
+        {
+            if (position[0] < 0 || position[0] > size - 1
+                || position[1] < 0 || position[1] > size - 1)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        static char[,] Mover(char[,] matrix, string command, int rowP, int colP)
+        {
+            if (command == "up")
+            {
+                matrix[rowP - 1, colP] = matrix[rowP, colP];
+                matrix[rowP, colP] = '-';
+            }
+            else if (command == "down")
+            {
+                matrix[rowP + 1, colP] = matrix[rowP, colP];
+                matrix[rowP, colP] = '-';
+            }
+            else if (command == "left")
+            {
+                matrix[rowP, colP - 1] = matrix[rowP, colP];
+                matrix[rowP, colP] = '-';
+            }
+            else if (command == "right")
+            {
+                matrix[rowP, colP + 1] = matrix[rowP, colP];
+                matrix[rowP, colP] = '-';
+            }
+
+            return matrix;
+        }
+
+        static void Printer(char[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int k = 0; k < matrix.GetLength(1); k++)
+                {
+                    Console.Write(matrix[i, k]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
