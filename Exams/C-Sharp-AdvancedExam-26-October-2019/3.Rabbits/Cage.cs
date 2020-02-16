93147119033040aed18ea2860f027ea167b9b856
Naiden Nikolaev Petrov <58266467+naidenpetrov00@users.noreply.github.com>
2020-02-16 18:01:25 +0200

    ExamExercise

---
+++ Exams/C-Sharp-AdvancedExam-26-October-2019/3.Rabbits/Cage.cs
@@ -0,0 +1,136 @@
+﻿namespace Rabbits
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Cage
+    {
+        private List<Rabbit> data;
+        private string name;
+        private int capacity;
+
+        public Cage(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.data = new List<Rabbit>(capacity);
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Capacity
+        {
+            get { return this.capacity; }
+            set { this.capacity = value; }
+        }
+
+        public int Count
+        {
+            get { return this.data.Count; }
+        }
+
+        public void Add(Rabbit rabbit)
+        {
+            if (this.data.Count < this.Capacity)
+            {
+                this.data.Add(rabbit);
+            }
+        }
+
+        public bool RemoveRabbit(string name)
+        {
+            if (!EmptyCageChecker(this.data))
+            {
+                return this.data
+                    .Remove(this.data.Find(rab => rab.Name.Equals(name)));
+            }
+            else
+            {
+                return default;
+            }
+        }
+
+        public void RemoveSpecies(string species)
+        {
+            if (!EmptyCageChecker(this.data))
+            {
+                this.data
+                .RemoveAll(n => n.Species.Equals(species));
+            }
+        }
+
+        public Rabbit SellRabbit(string name)
+        {
+            if (!EmptyCageChecker(this.data))
+            {
+                this.data
+                .Find(rab => rab.Name.Equals(name))
+                .Available = false;
+
+                return this.data
+                    .Find(rab => rab.Name.Equals(name));
+            }
+            else
+            {
+                return default;
+            }
+        }
+
+        public Rabbit[] SellRabbitsBySpecies(string species)
+        {
+            if (!EmptyCageChecker(this.data))
+            {
+                this.data
+                .FindAll(rab => rab.Species.Equals(species))
+                .Select(rab => rab.Available = false)
+                .ToList();
+
+                return this.data
+                    .Where(rab => rab.Species.Equals(species))
+                    .ToArray();
+            }
+            else
+            {
+                return default;
+            }
+        }
+
+        public string Report()
+        {
+            if (!EmptyCageChecker(this.data))
+            {
+                string output = $"Rabbits available at {this.Name}:";
+
+                foreach (var item in this.data)
+                {
+                    if (item.Available == true)
+                    {
+                        output += $"\n{item}";
+                    }
+                }
+
+                return output;
+            }
+            else
+            {
+                return default;
+            }
+        }
+
+        public bool EmptyCageChecker(List<Rabbit> data)
+        {
+            if (data.Count > 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Exams/C-Sharp-AdvancedExam-26-October-2019/3.Rabbits/Rabbit.cs
@@ -0,0 +1,38 @@
+﻿namespace Rabbits
+{
+    public class Rabbit
+    {
+        private string name;
+        private string species;
+        private bool available = true;
+
+        public Rabbit(string name, string species)
+        {
+            this.Name = name;
+            this.Species = species;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public string Species
+        {
+            get { return this.species; }
+            set { this.species = value; }
+        }
+
+        public bool Available
+        {
+            get { return this.available; }
+            set { this.available = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"Rabbit ({this.species}): {this.name}";
+        }
+    }
+}
