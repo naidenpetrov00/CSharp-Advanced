afae0ff28b857c1b27f4ce218aaf8123d36a9c19
Naiden Nikolaev Petrov <58266467+naidenpetrov00@users.noreply.github.com>
2020-07-02 20:53:46 +0300

    Exercise

---
+++ Exams/C-Sharp-Advanced-Exam-24-Feb-2019/ClubParty/Startup.cs
@@ -0,0 +1,28 @@
+﻿namespace ClubParty
+{
+    using System;
+    using System.Collections;
+    using System.Linq;
+
+    public class Startup
+    {
+        static void Main(string[] args)
+        {
+            var hallsMaximumCapacity = int.Parse(Console.ReadLine());
+            var questsCode = Console.ReadLine()
+                .Split(" ")
+                .ToArray();
+
+            var halls = new Stack();
+            var people = new Stack();
+
+            foreach (var item in questsCode)
+            {
+                if (item.GetType() == )
+                {
+
+                }
+            }
+        }
+    }
+}
