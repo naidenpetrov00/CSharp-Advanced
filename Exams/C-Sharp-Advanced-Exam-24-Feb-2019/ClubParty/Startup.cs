6ca961c7b99777cf9288403cfe7e69d48d1b91d4
Naiden Nikolaev Petrov <58266467+naidenpetrov00@users.noreply.github.com>
2020-07-20 21:28:17 +0300

    Exercise

--- Exams/C-Sharp-Advanced-Exam-24-Feb-2019/ClubParty/Startup.cs
+++ Exams/C-Sharp-Advanced-Exam-24-Feb-2019/ClubParty/Startup.cs
@@ -10,25 +10,46 @@
         static void Main(string[] args)
         {
             var hallsMaximumCapacity = int.Parse(Console.ReadLine());
+            //var line = Console.ReadLine()
+            //    .Split(" ")
+            //    .Reverse()
+            //    .ToArray();
             var line = Console.ReadLine()
-                .Split(" ")
-                .Reverse()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
                 .ToArray();
+
+            var reservation = StackAdder(line);
+            var openHalls = new Queue();
 
             var halls = new Dictionary<string, List<int>>();
             var hall = string.Empty;
+            var resCount = reservation.Count;
 
-            for (int i = 0; i < line.Length; i++)
+            for (int i = 0; i < resCount; i++)
             {
+                var resInfo = reservation.Pop().ToString();
 
-                if (int.TryParse(line[i], out int number) && halls.Count() > 0)
+                if (halls.Count == 0)
                 {
-                    halls.
+                    continue;
+                }
+
+                if (int.TryParse(resInfo, out int number))
+                {
+                    if (PlaceChecker(halls, openHalls, number, hallsMaximumCapacity))
+                    {
+
+                    }
+                }
+                else if (resInfo != null)
+                {
+                    halls.Add(resInfo, new List<int>());
+                    openHalls.Enqueue(resInfo);
                 }
             }
         }
 
-        static Stack StackAdder(string[] input)
+        private static Stack StackAdder(string[] input)
         {
             var stack = new Stack();
 
@@ -38,6 +59,23 @@
             }
 
             return stack;
+        }
+
+        private static bool PlaceChecker(Dictionary<string, List<int>> halls, Queue openHals, int number, int maxCapcity)
+        {
+            var capacity = 0;
+
+            foreach (var item in halls[openHals.Peek().ToString()])
+            {
+                capacity += item;
+            }
+
+            if (capacity + number > maxCapcity)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
