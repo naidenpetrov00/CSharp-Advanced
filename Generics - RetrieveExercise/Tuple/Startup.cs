f37d38e7d458415dd97c6a6d9aeb33334057cb5b
Naiden Nikolaev Petrov <58266467+naidenpetrov00@users.noreply.github.com>
2021-04-21 19:34:41 +0300

    RetrieveEcercise

---
+++ Generics - RetrieveExercise/Tuple/Startup.cs
@@ -0,0 +1,27 @@
+﻿namespace Tuple
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Startup
+    {
+        static void Main(string[] args)
+        {
+            var nameAdress = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            var nameBeerCapacity = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            var integerDouble = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            var integerInput = int.Parse(integerDouble[0]);
+            var doubleInput = double.Parse(integerDouble[1]);
+
+            var tupleOne = new MyTuple<>(nameAdress[0], nameAdress[1]);
+        }
+    }
+}
---
+++ Generics - RetrieveExercise/Tuple/Tuple.cs
@@ -0,0 +1,39 @@
+﻿namespace Tuple
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class MyTuple<T,K>
+    {
+        private T item1;
+        private K item2;
+
+        public MyTuple(T item1, K item2)
+        {
+            this.Item1 = item1;
+            this.Item2 = item2;
+        }
+
+        public T Item1
+        {
+            get { return this.item1; }
+            private set { this.item1 = value; }
+        }
+
+        public K Item2
+        {
+            get { return this.item2; }
+            private set { this.item2 = value; }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.Append(string.Format($"{0} > {1}", this.Item1, this.Item2));
+
+            return sb.ToString().Trim();
+        }
+    }
+}
