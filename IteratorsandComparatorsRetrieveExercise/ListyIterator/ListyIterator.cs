2a05048ecbe3d270e622dcad128a2dd5841c9df6
Naiden Nikolaev Petrov <58266467+naidenpetrov00@users.noreply.github.com>
2021-04-25 18:26:14 +0300

    RetriveExecise

---
+++ IteratorsandComparatorsRetrieveExercise/ListyIterator/ListyIterator.cs
@@ -0,0 +1,68 @@
+﻿namespace ListyIterator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class ListyIterator<T>
+    {
+        private List<T> data;
+        private int index;
+
+        public ListyIterator(params T[] data)
+        {
+            this.data = new List<T>(data);
+            this.Index = 0;
+        }
+
+        public IReadOnlyList<T> Data
+        {
+            get { return this.data; }
+        }
+
+        public int Count
+        {
+            get { return this.data.Count; }
+        }
+
+        public int Index
+        {
+            get { return this.index; }
+            private set { this.index = value; }
+        }
+
+        public bool Move()
+        {
+            if (this.Index >= this.Count - 1)
+            {
+                return false;
+            }
+
+            this.Index++;
+
+            return true;
+        }
+
+        public bool HasNext()
+        {
+            if (this.Index >= this.Count - 1)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void Print()
+        {
+            if (this.Count == 0)
+            {
+                throw new ArgumentNullException("Invalid Operation!");
+            }
+
+            Console.WriteLine(this.Data[this.Index]);
+        }
+
+
+    }
+}
---
+++ IteratorsandComparatorsRetrieveExercise/ListyIterator/Startup.cs
@@ -0,0 +1,28 @@
+﻿namespace ListyIterator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Startup
+    {
+        static void Main(string[] args)
+        {
+            var command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                Create(command);
+            }
+        }
+
+        public static void Create(string command)
+        {
+            var input = command
+                .Split(new string[] { "Create", " "}, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+
+        }
+    }
+}
