09030c3f217d660f62b4f64813066fdba50542f6
Naiden Nikolaev Petrov <58266467+naidenpetrov00@users.noreply.github.com>
2021-06-26 20:00:57 +0300

    RetrieveLab

---
+++ GenericsRetrieveLab/Box/Box.cs
@@ -0,0 +1,35 @@
+﻿namespace BoxOfT
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Box<T>
+    {
+        private List<T> data;
+
+        public Box()
+        {
+            this.data = new List<T>();
+        }
+
+        public int Count 
+        {
+            get => this.data.Count;
+        }
+
+        public void Add(T element)
+        {
+            this.data.Add(element);
+        }
+
+        public T Remove()
+        {
+            var element = this.data[this.Count - 1];
+            
+            this.data.Remove(element);
+
+            return element;
+        } 
+    }
+}
---
+++ GenericsRetrieveLab/GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,22 @@
+﻿namespace GenericArrayCreator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class ArrayCreator
+    {
+
+        public static T[] Create<T>(int lenght, T item)
+        {
+            var data = new T[lenght];
+
+            for (int i = 0; i < data.Length - 1; i++)
+            {
+                data[i] = item;
+            }
+
+            return data;
+        }
+    }
+}
