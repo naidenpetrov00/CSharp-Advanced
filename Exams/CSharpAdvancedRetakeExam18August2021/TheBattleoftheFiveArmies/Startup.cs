3b2b6f3e07e388b333d87d0828979fa9511ce7d6
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2021-09-22 22:06:29 +0300

    ExamExercise

---
+++ Exams/CSharpAdvancedRetakeExam18August2021/TheBattleoftheFiveArmies/Startup.cs
@@ -0,0 +1,48 @@
+﻿namespace TheBattleoftheFiveArmies
+{
+    using System;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var armyArmor = int.Parse(Console.ReadLine());
+            var numberOfRows = int.Parse(Console.ReadLine());
+
+            char[,] world = MatrixCreator(numberOfRows);
+
+            var command = new string[] { };
+
+
+            while (true)
+            {
+                 command = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+
+            }
+
+
+        }
+
+        private static char[,] MatrixCreator(int numberOfRows)
+        {
+            char[,] world = new char[numberOfRows, numberOfRows];
+            var rows = new char[numberOfRows];
+
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                rows = Console.ReadLine().ToCharArray();
+
+                for (int k = 0; k < numberOfRows; k++)
+                {
+                    world[i, k] = rows[k];
+                }
+            }
+
+            return world;
+        }
+    }
+}
