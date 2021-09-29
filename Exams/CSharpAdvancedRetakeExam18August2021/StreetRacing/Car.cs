709b6f649740d641c9a789480b2747b0ac5783aa
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2021-09-29 21:44:41 +0300

    ExamExercise

---
+++ Exams/CSharpAdvancedRetakeExam18August2021/StreetRacing/Car.cs
@@ -0,0 +1,68 @@
+﻿namespace StreetRacing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Car
+    {
+        private string make;
+        private string model;
+        private string licensePlate;
+        private int horsePower;
+        private double weight;
+
+        public Car(string make, string model, string licensePlate, int horsePower, double weight)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.LicensePlate = licensePlate;
+            this.HorsePower = horsePower;
+            this.Weight = weight;
+        }
+
+        public string Make
+        {
+            get { return this.make; }
+            private set { this.make = value; }
+        }
+
+        public string Model
+        {
+            get { return this.model; }
+            private set { this.model = value; }
+        }
+
+        public string LicensePlate
+        {
+            get { return this.licensePlate; }
+            private set { this.licensePlate = value; }
+        }
+
+        public int HorsePower
+        {
+            get { return this.horsePower; }
+            private set { this.horsePower = value; }
+        }
+
+        public double Weight
+        {
+            get { return this.weight; }
+            private set { this.weight = value; }
+        }
+
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"License Plate: {this.LicensePlate}");
+            sb.AppendLine($"Horse Power: {this.HorsePower}");
+            sb.AppendLine($"Weight: {this.Weight}");
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ Exams/CSharpAdvancedRetakeExam18August2021/StreetRacing/Race.cs
@@ -0,0 +1,132 @@
+﻿namespace StreetRacing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+
+    public class Race
+    {
+        private List<Car> participants;
+        private string name;
+        private string type;
+        private int laps;
+        private int capacity;
+        private int maxHorsePower;
+
+
+        public Race(string name, string type, int laps, int capacity, int maxHorsePower)
+        {
+            this.participants = new List<Car>();
+            this.Name = name;
+            this.Type = type;
+            this.Laps = laps;
+            this.Capacity = capacity;
+            this.MaxHorsePower = maxHorsePower;
+        }
+
+
+        public string Name
+        {
+            get { return this.name; }
+            private set { this.name = value; }
+        }
+        public string Type
+        {
+            get { return this.type; }
+            private set { this.type = value; }
+        }
+        public int Laps
+        {
+            get { return this.laps; }
+            private set { this.laps = value; }
+        }
+        public int Capacity
+        {
+            get { return this.capacity; }
+            private set { this.capacity = value; }
+        }
+        public int MaxHorsePower
+        {
+            get { return this.maxHorsePower; }
+            private set { this.maxHorsePower = value; }
+        }
+        public IReadOnlyCollection<Car> Participants
+        {
+            get { return this.participants; }
+        }
+        public int Count
+        {
+            get { return this.Participants.Count; }
+        }
+
+
+        public void Add(Car car)
+        {
+            if (!this.participants.Contains(car) && this.participants.Count < this.Capacity && car.HorsePower <= this.MaxHorsePower)
+            {
+                this.participants.Add(car);
+            }
+        }
+        public bool Remove(string licensePlate)
+        {
+            return CarChecker(licensePlate);
+        }
+        public Car FindeParticipant(string licensePlate)
+        {
+            if (CarChecker(licensePlate))
+            {
+                return this.participants.Find(c => c.LicensePlate.Equals(licensePlate));
+            }
+
+            return null;
+        }
+        public Car GetMostPowerfulCar()
+        {
+            if (this.Participants.Count == 0)
+            {
+                return null;
+            }
+
+            var maxPower = 0;
+
+            foreach (var item in this.participants)
+            {
+                if (item.HorsePower > maxPower)
+                {
+                    maxPower = item.HorsePower;
+                }
+            }
+
+            return this.participants.Find(c => c.HorsePower.Equals(maxPower));
+        }
+
+
+        private bool CarChecker(string licensePlate)
+        {
+            foreach (var item in this.participants)
+            {
+                if (item.LicensePlate.Equals(licensePlate))
+                {
+                    this.participants.Remove(item);
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        public string Report()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"Race: {this.Name} - Type: {this.Type} (Laps: {this.Laps})");
+
+            this.participants.ForEach(c => sb.AppendLine(c.ToString()));
+
+            return sb.ToString().Trim();
+
+        }
+    }
+}
